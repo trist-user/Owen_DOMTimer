9206968fc96cc54a6c6fbf1207b86720b660f66c
Tristan Owen <towen1@my.waketech.edu>
2026-02-24 14:09:43 -0500

    hahahahahahahahahahahaha

---
+++ timer.js
@@ -0,0 +1,9 @@
+
+strt = document.getElementById("startTimer");
+stp = document.getElementById("stopTimer");
+rst = document.getElementById("resetTimer");
+
+let timer = document.createElement("h2");
+timer.innerHTML = "Timer";
+
+strt.addEventListener
